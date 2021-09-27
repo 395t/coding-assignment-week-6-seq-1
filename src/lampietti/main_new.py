01436fdfda99ade47bea761ff75f24e9dccd6275
Elias Lampietti <elampietti@utexas.edu>
2021-09-27 18:55:33 -0500

    param updates

--- src/lampietti/main_new.py
+++ src/lampietti/main_new.py
@@ -19,9 +19,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 src_langs = ['en', 'de', 'it']
 tgt_langs = ['de', 'it', 'en']
 
-subset_size = 25
-batch_size = 5
-num_epochs = 1
+subset_size = 25000
+batch_size = 32
+num_epochs = 10
 embed_dim = 256
 hidden_dim = 512
 n_layers = 2
